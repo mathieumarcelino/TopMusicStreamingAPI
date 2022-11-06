9f284ff9fc5f575039f47356b559389ab15d9c8f
lucasnevespereira <pereiraneveslucas@gmail.com>
2022-11-06 12:17:02 +0100

    add env file

--- utils/config.go
+++ utils/config.go
@@ -1,23 +1,30 @@
 package utils
 
-import "os"
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
 
 type Config struct {
-	Env string
-	Port int
+	Env     string
+	Port    int
 	AppName string
 }
 
-
 func LoadConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		Logger.Fatal("could not load .env file")
+	}
+
 	conf := Config{}
 
 	conf.AppName = "TopMusicStreaming API"
 	conf.Port = 9990
 	conf.Env = PROD
 
-	if os.Getenv("env") != EMPTY {
-		conf.Env = os.Getenv("env")
+	if os.Getenv("ENV") != EMPTY {
+		conf.Env = os.Getenv("ENV")
 	}
 
 	return conf
